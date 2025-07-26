8ee2b5a132d061ed785b7b5e501368f5c1de7583
Vinicius Oliveira <oliveira.viniciuss@outlook.com>
2025-07-26 16:17:07 -0300

    chore: o .git ignore, inclui o .data/

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissoesModel.java
@@ -19,6 +19,8 @@ public class MissoesModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @Enumerated(EnumType.STRING)
     private RankMissoes rank;
 
     // @OneToMany - Uma missão pode ter vários ninjas
